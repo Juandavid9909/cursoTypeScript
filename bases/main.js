f8c5a08219761b90d874724c3503d2ce1deb954c
Juan David <juandavid918@gmail.com>
2022-02-19 18:04:50 -0500

    Seccion genericos finalizada

--- bases/main.js
+++ bases/main.js
@@ -1,66 +1,12 @@
 "use strict";
-(() => {
-    ;
-    let flash = {
-        name: "Barry Allen",
-        age: 24,
-        powers: ["Super velocidad", "Viajar en el tiempo"]
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return text.length > 3 ? true : false;
     };
-    let superman = {
-        name: "Clark Kent",
-        age: 60,
-        powers: ["Super velocidad"],
-        getName() {
-            return this.name;
-        }
+    const validateDate = (myDate) => {
+        return isNaN(myDate.valueOf()) ? false : true;
     };
-})();
-(() => {
-    ;
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return this.name + " " + this.realName;
-        }
-    }
-})();
-(() => {
-    ;
-    ;
-    const client = {
-        name: "Juan David",
-        age: 22,
-        address: {
-            id: 125,
-            zip: "KY2 SUD",
-            city: "Ottawa"
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: "Fernando",
-        age: 30,
-        address: {
-            city: "Toronto",
-            id: 120,
-            zip: "K2S U23"
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    ;
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText("Jua"));
 //# sourceMappingURL=main.js.map
---
+++ ts-node/src/backs/copia.ts
@@ -0,0 +1,14 @@
+// import { Hero as SuperHero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import powers, { Power } from "./data/powers";
+import { Hero } from "../classes/Hero";
+
+// const Hero = 123;
+
+// const ironman = new SuperHero("Iron Man", 1, 55);
+// const ironman = new HeroClasses.Hero("Iron Man", 1, 55);
+const ironman = new Hero("Iron Man", 1, 55);
+
+console.log(ironman.power);
+
+// console.log(powers);
